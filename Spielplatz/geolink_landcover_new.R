fbd24b07c3adbd83300cc5ecab07d1c7033465ef
DianaJaganjac <diana.jaganjac@hotmail.com>
2024-08-20 11:49:46 +0200

    fixed landcover and population
    fixed landcover and population

---
+++ Spielplatz/geolink_landcover_new.R
@@ -0,0 +1,112 @@
+pacman::p_load(rstac, reticulate, terra, raster, osmdata, sp, sf, geodata, httr, ncdf4, rgdal, exactextractr, parallel)
+
+
+geolink_landcover <- function(time_unit = "annual",
+                              start_date,
+                              end_date,
+                              shp_dt) {
+
+
+  start_date <- as.Date(start_date)
+  end_date <- as.Date(end_date)
+
+  s_obj <- stac("https://planetarycomputer.microsoft.com/api/stac/v1")
+
+  it_obj <- s_obj %>%
+    stac_search(collections = "io-lulc-annual-v02",
+                bbox = sf::st_bbox(shp_dt),
+                datetime = paste(start_date, end_date, sep = "/")) %>%
+    get_request() %>%
+    items_sign(sign_fn = sign_planetary_computer())
+
+  url_list <- lapply(1:length(it_obj$features),
+                     function(x) {
+                       url <- paste0("/vsicurl/", it_obj$features[[x]]$assets$data$href)
+                       return(url)
+                     })
+
+  # Load rasters without projecting initially
+  raster_objs <- lapply(url_list, terra::rast)
+
+  # Name rasters
+  raster_list <- lapply(seq_along(raster_objs), function(i) {
+    setNames(raster_objs[[i]], as.character(i))
+  })
+
+  # Ensure shapefile CRS matches raster CRS for consistency
+  raster_crs <- terra::crs(raster_objs[[1]])
+  shp_dt_transformed <- st_transform(shp_dt, crs = raster_crs)
+
+  # Filter raster objects using the filter_tiles function
+  raster_objs <- filter_tiles_landcover(raster_list, dt = shp_dt_transformed)
+
+
+  # Extract file values
+  file_values <- it_obj$features[[1]]$assets$data$`file:values`
+
+  # Initialize vectors for class names and class values
+  class_names <- c()
+  class_values <- c()
+
+  # Iterate over the list to populate class names and values
+  for (item in file_values) {
+    class_values <- c(class_values, item$values)
+    class_names <- c(class_names, item$summary)
+  }
+
+  # Create a named vector for class values with class names as names
+  class_values_named <- setNames(class_values, class_names)
+
+  # Initialize a list to store proportions for each class
+  proportions_list <- list()
+
+  # Apply the summarizing function to each filtered raster and combine results
+  for (i in seq_along(raster_objs)) {
+    print(paste("Processing raster:", i))
+
+    # Extract values from raster that intersect with transformed shapefile
+    extracted_values <- exact_extract(raster_objs[[i]], shp_dt_transformed, coverage_area = TRUE)
+
+    # Debug: Check the extracted values structure
+    if (length(extracted_values) == 0) {
+      warning("No values extracted for raster ", i)
+      proportions_list[[i]] <- rep(0, length(class_values))
+      next
+    }
+
+    # Calculate total coverage area for normalization
+    total_area <- sum(sapply(extracted_values, function(ev) sum(ev$coverage_area, na.rm = TRUE)))
+
+    if (total_area == 0) {
+      warning("Total area is zero for raster ", i)
+      proportions_list[[i]] <- rep(0, length(class_values))
+      next
+    }
+
+    # Summarize the extracted values into proportions for each class
+    class_proportions <- sapply(class_values, function(class_val) {
+      class_area <- sum(sapply(extracted_values, function(ev) {
+        sum(ev$coverage_area[ev$value == class_val], na.rm = TRUE)
+      }))
+      class_proportion <- (class_area / total_area)*100
+      return(class_proportion)
+    })
+
+    # Append the proportions for the current raster to the list
+    proportions_list[[i]] <- class_proportions
+  }
+
+  # Combine the proportions for all rasters into a data frame
+  proportions_df <- do.call(rbind, proportions_list)
+
+  # Set column names to class names
+  colnames(proportions_df) <- class_names
+
+  # Return the proportions table along with other information
+  return(proportions_df)
+}
+
+# Example usage (assuming shp_dt is correctly loaded as an sf object with a column named ADM1_EN)
+df <- geolink_landcover(start_date = "2020-01-01",
+                        end_date = "2020-03-01",
+                        shp_dt = shp_dt[shp_dt$ADM1_EN == "Abia",])
---
+++ Spielplatz/geolink_population_new.R
@@ -0,0 +1,120 @@
+pacman::p_load(rstac, reticulate, terra, raster, osmdata, sp, sf, geodata, rvest, httr)
+
+geolink_population <- function(start_year = NULL,
+                               end_year = NULL,
+                               iso_code,
+                               UN_adjst = NULL,
+                               constrained = NULL,
+                               bespoke = NULL,
+                               version = NULL,
+                               shp_dt = NULL,
+                               shp_fn = NULL,
+                               grid_size = 1000,
+                               survey_dt,
+                               survey_fn = NULL,
+                               survey_lat = NULL,
+                               survey_lon = NULL,
+                               buffer_size = NULL,
+                               extract_fun = "mean",
+                               survey_crs = 4326,
+                               file_location = tempdir()) {
+
+  if (!dir.exists(file_location)) {
+    dir.create(file_location, recursive = TRUE)
+  }
+
+  if (!is.null(start_year) && !is.null(end_year)) {
+    years <- seq(start_year, end_year)
+  }
+
+  if (!is.null(constrained) && constrained == "Y") {
+    url1 <- paste0("https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/2020/BSGM/", iso_code, "/")
+    url2 <- paste0("https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/2020/maxar_v1/", iso_code, "/")
+
+    file_urls <- try_download(url1)
+    if (is.null(file_urls)) {
+      file_urls <- try_download(url2)
+    }
+
+    if (!is.null(file_urls)) {
+      download_files_worldpop(file_urls, UN_adjst, file_location)
+    } else {
+      warning("No files found at both URLs.")
+    }
+  } else {
+    if (!is.null(bespoke) && bespoke == "Y") {
+      url <- paste0("https://data.worldpop.org/repo/wopr/", iso_code,
+                    "/population/v", version, "/", iso_code, "_population_v",
+                    gsub("\\.", "_", version), "_mastergrid.tif")
+      download.file(url, file.path(file_location, basename(url)))
+    } else {
+      if (!is.null(start_year) && !is.null(end_year)) {
+        for (year in years) {
+          url <- paste0("https://data.worldpop.org/GIS/Population/Global_2000_2020/", year, "/", iso_code, "/")
+
+          file_urls <- try_download(url)
+
+          if (!is.null(file_urls)) {
+            download_files_worldpop(file_urls, UN_adjst, file_location)
+          } else {
+            warning(paste("No files found for year", year, "at URL", url))
+          }
+        }
+      }
+    }
+  }
+
+  tif_files <- list.files(file_location, pattern = "\\.tif$", full.names = TRUE)
+
+  raster_objs <- lapply(tif_files, function(x) {
+    tryCatch({
+      terra::rast(x)
+    }, error = function(e) {
+      warning(paste("Failed to read:", x, "with error:", e))
+      return(NULL)
+    })
+  })
+
+  raster_objs <- raster_objs[!sapply(raster_objs, is.null)]
+
+  if (length(raster_objs) == 0) {
+    stop("No valid raster files found.")
+  }
+
+  # Define the name_set at the end
+  if (!is.null(bespoke) && bespoke == "Y") {
+    name_set <- paste0("population_", version)
+  } else if (!is.null(start_year) && !is.null(end_year)) {
+    name_set <- paste0("population_", years)
+  } else {
+    name_set <- "population_2020"
+  }
+
+  print("Population Raster Downloaded")
+
+  dt <- postdownload_processor(shp_dt = shp_dt,
+                               raster_objs = raster_objs,
+                               shp_fn = shp_fn,
+                               grid_size = grid_size,
+                               survey_dt = survey_dt,
+                               survey_fn = survey_fn,
+                               survey_lat = survey_lat,
+                               survey_lon = survey_lon,
+                               extract_fun = extract_fun,
+                               buffer_size = buffer_size,
+                               survey_crs = survey_crs,
+                               name_set = name_set)
+
+  print("Process Complete!!!")
+
+  return(dt)
+}
+
+# Example usage
+df <- geolink_population(iso_code = "NGA",
+                         UN_adjst = "N",
+                         constrained = "Y",
+                         shp_dt = shp_dt[shp_dt$ADM1_EN == "Abia",],
+                         grid_size = 1000,
+                         extract_fun = "mean",
+                         file_location = "C:/Users/Diana Jaganjac/Documents/New folder")
