ccfcca5a4a3787de856bdd314cb462809646e01d
DianaJaganjac~ <diana.jaganjac@hotmail.com~>
2025-05-30 11:14:57 -0400

    function calls added

--- Spielplatz/geolink_chirps.R
+++ Spielplatz/geolink_chirps.R
@@ -37,6 +37,35 @@ df <- geolink_chirps(time_unit = "month",
                      extract_fun = "weighted_mean",
                      weight_raster = pop_raster)
 
+df2 <- geolink_chirps(time_unit = "month",
+                     start_date = "2020-01-01",
+                     end_date = "2020-03-01",
+                     shp_fn = "tests/testthat/testdata/shp_dt.shp",
+                     grid_size = 1000,
+                     extract_fun = "weighted_mean",
+                     weight_raster = pop_raster)
+
+df6 <- geolink_chirps(time_unit = "month",
+                     start_date = "2020-01-01",
+                     end_date = "2020-03-01",
+                     survey_dt = hhgeo_dt[hhgeo_dt$ADM1_PCODE == "NG001",],
+                     grid_size = 1000,
+                     extract_fun = "weighted_mean",
+                     weight_raster = pop_raster)
+
+df5 <- geolink_chirps(time_unit = "month",
+                     start_date = "2020-01-01",
+                     end_date = "2020-03-01",
+                     survey_fn = "tests/testthat/testdata/xy_hhgeo_dt.dta",
+                     survey_lon = "x",
+                     survey_lat = "y",
+                     grid_size = 1000,
+                     extract_fun = "weighted_mean",
+                     weight_raster = pop_raster)
+
+
+
+
 
 #### lets map the plot
 
